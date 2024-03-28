696f63dc814a12b820d3b3a91c0518c5e8a5ab30
Angel Cancio <7303724+4ng31@users.noreply.github.com>
2024-03-28 17:52:20 -0300

    Fixing the datasync policy

--- data.tf
+++ data.tf
@@ -10,13 +10,13 @@ data "aws_iam_policy_document" "datasync_assume" {
     actions = ["sts:AssumeRole"]
 
     principals {
-      type        = "Service"
-      identifiers = ["datasync.amazonaws.com"]
+        type        = "Service"
+        identifiers = ["datasync.amazonaws.com"]
     }
 
     principals {
-      type        = "Service"
-      identifiers = ["s3.amazonaws.com"]
+        type        = "Service"
+        identifiers = ["s3.amazonaws.com"]
     }
   }
 }
@@ -28,18 +28,27 @@ data "aws_iam_policy_document" "datasync" {
     actions = [ 
         "s3:GetBucketLocation",
         "s3:ListBucket",
-        "s3:ListBucketMultipartUploads",
-        "s3:GetBucketLocation",
+        "s3:ListBucketMultipartUploads"
+    ]
+    resources = [
+        "arn:aws:s3:::${var.src_s3_bucket_name}",
+        "arn:aws:s3:::${var.dst_s3_bucket_name}"
+        ]
+    }
+
+  statement {
+    effect = "Allow"
+    actions = [
         "s3:AbortMultipartUpload",
         "s3:DeleteObject",
         "s3:GetObject",
         "s3:ListMultipartUploadParts",
-        "s3:PutObject",
+        "s3:PutObjectTagging",
         "s3:GetObjectTagging",
-        "s3:PutObjectTagging"
-        ]
+        "s3:PutObject"
+    ]
     resources = [
-        "arn:aws:s3:::${var.dst_s3_bucket_name}",
+        "arn:aws:s3:::${var.src_s3_bucket_name}",
         "arn:aws:s3:::${var.dst_s3_bucket_name}/*"
         ]
     }
@@ -50,26 +59,30 @@ data "aws_iam_policy_document" "datasync" {
 data "aws_iam_policy_document" "dst_s3_attached_policy" {
   statement {
     principals {
-      type        = "AWS"
-      identifiers = [var.src_iam_role_arn]
+        type        = "AWS"
+        identifiers = [var.src_iam_role_arn]
     }
 
     actions = [
-      "s3:GetBucketLocation",
-      "s3:ListBucket",
-      "s3:ListBucketMultipartUploads",
-      "s3:AbortMultipartUpload",
-      "s3:DeleteObject",
-      "s3:GetObject",
-      "s3:ListMultipartUploadParts",
-      "s3:PutObject",
-      "s3:GetObjectTagging",
-      "s3:PutObjectTagging"
+        "s3:ListBucket",
+        "s3:ListBucketVersions",
+        "s3:GetBucketAcl",
+        "s3:PutObject",
+        "s3:PutObjectAcl",
+        "s3:GetObject",
+        "s3:GetObjectAcl",
+        "s3:DeleteObject",
+        "s3:AbortMultipartUpload",
+        "s3:GetBucketLocation",
+        "s3:ListBucketMultipartUploads",
+        "s3:ListMultipartUploadParts",
+        "s3:GetObjectTagging",
+        "s3:PutObjectTagging"
     ]
 
     resources = [
-      "arn:aws:s3:::${var.dst_s3_bucket_name}",
-      "arn:aws:s3:::${var.dst_s3_bucket_name}/*"
+        "arn:aws:s3:::${var.dst_s3_bucket_name}",
+        "arn:aws:s3:::${var.dst_s3_bucket_name}/*"
     ]
   }
 }
